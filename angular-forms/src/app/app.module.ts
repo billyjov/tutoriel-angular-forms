763bababd1e24d55a90e19cd51c70183b0c0af9c
Billy Lando <billy_lando@yahoo.com>
2020-11-17 14:53:54 +0100

    feat(part-2): add code block for part-2

--- angular-forms/src/app/app.module.ts
+++ angular-forms/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
