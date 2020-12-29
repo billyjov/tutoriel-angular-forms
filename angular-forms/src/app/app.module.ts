98efa4b7bfd030339776e65f5180a7deffa65d53
Billy Lando <billy_lando@yahoo.com>
2020-12-29 12:34:50 +0100

    feat: replace module to include reactivity

--- angular-forms/src/app/app.module.ts
+++ angular-forms/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +12,7 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
