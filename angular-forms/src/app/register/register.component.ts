fd4dd078445b2147d33b08fdb65f1e706ec372b9
Billy Lando <billy_lando@yahoo.com>
2020-12-29 12:27:01 +0100

    feat: add initial changes to component.ts

--- angular-forms/src/app/register/register.component.ts
+++ angular-forms/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl} from '@angular/forms';
 import { User } from './user';
 
 @Component({
@@ -9,16 +9,23 @@ import { User } from './user';
 })
 export class RegisterComponent implements OnInit {
 
+  public registerForm: FormGroup;
   public user: User = new User();
 
   constructor() { }
 
   ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      email: new FormControl(),
+      sendCatalog: new FormControl(true)
+    });
   }
 
-  public saveData(registerForm: NgForm) {
-    console.log(registerForm.form);
-    console.log('valeurs: ', JSON.stringify(registerForm.value));
+  public saveData() {
+    console.log(this.registerForm);
+    console.log('valeurs: ', JSON.stringify(this.registerForm.value));
     console.log('hello');
   }
 }
